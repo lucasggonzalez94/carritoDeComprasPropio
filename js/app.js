94dbd9fb7a90e2591b3d012f0b8c6c6cfc9499bd
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2021-01-04 01:15:03 -0300

    Arreglos varios

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
-const botonesAgregar = document.querySelectorAll('.articulo .btn')
+const botonesAgregar = document.querySelectorAll('.curso .btn-agregar')
 const ventanaCarrito = document.querySelector('#ventana-carrito')
 const tablaCursos = document.querySelector('#body-tabla-cursos')
+const menuResponsive = document.querySelector('.icon-menu')
+const enlacesMenu = document.querySelector('#enlaces-menu')
+const header = document.querySelector('#header')
 
+const topHeader = header.offsetTop
 let carrito = []
 
 // Agregar listeners
 cargarEventListeners()
+
 function cargarEventListeners() {
     botonesAgregar.forEach(boton => {
         boton.addEventListener('click', leerArticulo)
     });
 
     ventanaCarrito.addEventListener('click', accionesCarrito)
+    document.addEventListener('click', esconderMenuResponsive)
+    window.addEventListener('scroll', navBlack)
 }
 
 // Al hacer click en el boton añadir se crea un objeto con la info del articulo y se agrega al carrito
 function leerArticulo(e) {
     e.preventDefault()
 
-    const curso = e.target.parentElement.parentElement
+    const curso = e.target.parentElement.parentElement.parentElement
 
     const cursoObj = {
         id: e.target.dataset.id,
         titulo: curso.querySelector('h3').textContent,
         precio: curso.querySelector('.precio').textContent,
-        imagen: curso.querySelector('img').src,
+        imagen: curso.querySelector('.img').src,
         cantidad: 1
     }
 
@@ -32,14 +39,14 @@ function leerArticulo(e) {
     const existe = carrito.some(curso => curso.id === cursoObj.id)
 
     if (existe) {
-        const cursos = carrito.map( curso => {
+        const cursos = carrito.map(curso => {
             if (curso.id === cursoObj.id) {
                 curso.cantidad++
                 return curso // Retorna el objeto actualizado
-            }else{
+            } else {
                 return curso // Retorna los objetos que no son duplicados
             }
-        } )
+        })
         carrito = [...cursos]
     } else {
         carrito.push(cursoObj)
@@ -54,22 +61,32 @@ function agregarArticuloHtml(cursos) {
     limpiarHTML(tablaCursos)
 
     cursos.forEach(curso => {
+
+        // Destructuring del curso a agregar
+        const {
+            imagen,
+            titulo,
+            precio,
+            cantidad,
+            id
+        } = curso
+
         tablaCursos.innerHTML += `
             <tr>
-                <td class="imagen"><img src="${curso.imagen}" alt="imagen" width="100px"/></td>
-                <td class="titulo-curso">${curso.titulo}</td>
-                <td class="precio-curso">${curso.precio}</td>
-                <td class="cantidad-curso">${curso.cantidad}</td>
+                <td class="imagen"><img src="${imagen}" alt="imagen" width="100px"/></td>
+                <td class="titulo-curso">${titulo}</td>
+                <td class="precio-curso">${precio}</td>
+                <td class="cantidad-curso">${cantidad}</td>
                 <td class="acciones">
-                    <a href="#" description="Eliminar" class="btn btn-eliminar eliminar-curso" data-id="${curso.id}">X</a>
+                    <a href="#" description="Eliminar" class="btn btn-eliminar eliminar-curso" data-id="${id}">X</a>
                 </td>
             </tr>`
 
-        ventanaCarrito.querySelector('#tabla-articulos').appendChild(tablaCursos)
+        ventanaCarrito.querySelector('#tabla-cursos').appendChild(tablaCursos)
     });
 }
 
-// Limpia elementos existentes
+// Limpia elementos existentes en el html
 function limpiarHTML(elemento) {
     while (elemento.firstChild) {
         elemento.removeChild(elemento.firstChild)
@@ -78,6 +95,7 @@ function limpiarHTML(elemento) {
 
 // Ejecuta las acciones de eliminar cursos o vaciar el carrito
 function accionesCarrito(e) {
+    e.preventDefault()
 
     if (e.target.classList.contains('btn-eliminar')) {
         const id = e.target.dataset.id
@@ -96,6 +114,37 @@ function vaciarCarrito() {
     limpiarHTML(tablaCursos)
 
     carrito = []
+}
+
+// MENU RESPONSIVE
+
+// Esconde o muestra el menu responsive
+function toggleMenu() {
+    existeClaseEsconder = enlacesMenu.classList.contains('esconder')
+
+    if (existeClaseEsconder) {
+        enlacesMenu.classList.remove('esconder')
+    } else {
+        enlacesMenu.classList.add('esconder')
+    }
+}
+
+// Al hacer click fuera del menu se esconde
+function esconderMenuResponsive(e) {
+    if (e.target === menuResponsive) {
+        toggleMenu()
+    } else if (e.target !== enlacesMenu) {
+        enlacesMenu.classList.add('esconder')
+    }
+}
+
+// Cuando se hace scroll se cambia el color de fondo de la barra de navegacion
+function navBlack() {
+    if (window.pageYOffset > topHeader) {
+        header.classList.add("fixed")
+    }else if (window.pageYOffset === topHeader) {
+        header.classList.remove("fixed");
+    }
 }
 
 // LISTA DE TAREAS
